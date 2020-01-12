3e1e6eac4f749185208e2e0037c259af4f762877
ellima <me@4ellingers.de>
2020-01-12 16:59:10 +0100

    Edit buttons next to answers, no function yet

--- public/js/waitforanswers.js
+++ public/js/waitforanswers.js
@@ -27,6 +27,7 @@ apiRequestJson("db", ["allAnswersAdmin", myName], function (res) {
             var nameToDisplay = decodeURIComponent(response[i].Name);
             if (myName == nameToDisplay){
                 document.getElementById("correctAnswer").textContent = "Korrekte Antwort: " + decodeURIComponent(response[i].Answer);
+                document.getElementById("correctAnswer").appendChild(editButton());
                 continue;
             }
             var answerAndName = nameToDisplay + ": " + decodeURIComponent(response[i].Answer);
@@ -34,6 +35,7 @@ apiRequestJson("db", ["allAnswersAdmin", myName], function (res) {
             var entryContent = document.createTextNode(answerAndName);
             
             listEntry.appendChild(entryContent);
+            listEntry.appendChild(editButton());
             document.getElementById("answers").appendChild(listEntry);
         }
         var gatheredAnswers = document.getElementById("answers").childElementCount;
@@ -56,6 +58,8 @@ function listenToAnswers(pnumber){
             var entryContent = document.createTextNode(answerAndName);
             
             listEntry.appendChild(entryContent);
+            listEntry.appendChild(editButton());
+            
             document.getElementById("answers").appendChild(listEntry);
             var gatheredAnswers = document.getElementById("answers").childElementCount;
             if (gatheredAnswers >= pnumber - 1) {
@@ -73,4 +77,14 @@ function listenToClearance(){
             window.location.href = "results.html";
         }
     });
+}
+
+function editButton() {
+    let newButton = document.createElement("button");
+    newButton.style = "margin-left: 10px";
+    
+    let buttonText = document.createTextNode("Bearbeiten");
+    newButton.appendChild(buttonText);
+
+    return newButton;
 }
